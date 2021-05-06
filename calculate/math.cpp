46f66613fad95abd9ed21bb950181400cb7b6338
eliasall3x <elliasallex@gmail.com>
2021-05-06 20:07:40 -0300

    feat: functions

--- calculate/math.cpp
+++ calculate/math.cpp
@@ -1,11 +1,16 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <locale.h>
+#include <stdio.h>  // printf
+#include <locale.h> // formatação de texto
 
-void soma();
-void subtracao();
-void multiplicacao();
+void soma(float num1, float num2);
+void subtracao(float num1, float num2);
+void multiplicacao(float num1, float num2);
+void divisao(float num1, float num2);
+void exibirResultado();
+void recebeOpcaoMenu(int opcao, float num1, float num2);
 void menu();
+
+float resultado;
 
 int main()
 {
@@ -19,35 +24,90 @@ void menu()
 {
 
   int opcao;
+  float numero1, numero2;
 
   do
   {
-    printf("Seleciona a opcao desejada");
+    system("cls");
+    printf("Calculadora avançada do 1SEM de SI");
+    printf("\n\t [1] - Soma");
+    printf("\n\t [2] - Subtração");
+    printf("\n\t [3] - Multiplicação");
+    printf("\n\t [4] - Divisão");
 
-    switch (opcao)
+    printf("\n\n Seleciona a opcao desejada: ");
+    scanf("%d%*c", &opcao);
+
+    if (opcao == 0)
     {
-    case 1:
-      soma();
-      break;
-
-    case 2:
-      subtracao();
-      break;
-
-    case 3:
-      multiplicacao();
-      break;
-
-    default:
+      printf("Programa finalizado!!");
       break;
     }
+
+    printf("\n\n Informe o primeiro valor: ");
+    scanf("%f", &numero1);
+    printf("Informe o segundo valor: ");
+    scanf("%f", &numero2);
+
+    recebeOpcaoMenu(opcao, numero1, numero2);
   } while (opcao != 0);
 }
 
-void soma()
+void recebeOpcaoMenu(int opcao, float num1, float num2)
+{
+  switch (opcao)
+  {
+  case 1:
+    soma(num1, num2);
+    break;
+
+  case 2:
+    subtracao(num1, num2);
+    break;
+
+  case 3:
+    multiplicacao(num1, num2);
+    break;
+
+  case 4:
+    divisao(num1, num2);
+    break;
+
+  default:
+    printf("Digite uma opcao valida!");
+    break;
+  }
+}
+
+void soma(float num1, float num2)
 {
   system("cls");
-  int soma = 2 + 10;
-  printf("Soma: %d", soma);
+  resultado = num1 + num2;
+  exibirResultado();
+}
+
+void subtracao(float num1, float num2)
+{
+  system("cls");
+  resultado = num1 - num2;
+}
+
+void multiplicacao(float num1, float num2)
+{
+  system("cls");
+  resultado = num1 * num2;
+  exibirResultado();
+}
+
+void divisao(float num1, float num2)
+{
+  system("cls");
+  resultado = num1 / num2;
+  exibirResultado();
+}
+
+void exibirResultado()
+{
+  printf("O resultado �: %f", resultado);
   system("pause");
 }
