457a30dd7679f6260ee9f96a36a8063fd46037bb
eliasall3x <elliasallex@gmail.com>
2021-06-17 19:42:05 -0300

    feat: test_ - 1_p2_exerc_02

--- tests_/part_2/exerc_02.cpp
+++ tests_/part_2/exerc_02.cpp
@@ -13,6 +13,32 @@ using namespace std;
 int main()
 {
   setlocale(LC_ALL, "Portuguese");
+  int matriz[8][8], maior = 0, linha, coluna;
+
+  for (int i = 0; i < 8; i++)
+  {
+    printf("- - - - - INSERE OS VALORES - - - - -\n");
+    printf("- - - - - - - - - - - - - - - - - - -\n");
+    for (int c = 0; c < 8; c++)
+    {
+      printf("[%d] X [%d] = ", i + 1, c + 1);
+      scanf("%d", &matriz[i][c]);
+
+      if (matriz[i][c] > maior || maior == 0)
+      {
+        maior = matriz[i][c];
+        linha = i + 1;
+        coluna = c + 1;
+      }
+    }
+    system("cls");
+  }
+
+  printf("- - - - - - - - - - - - - - - - - - -\n");
+  printf("\nMaior numero digitado: %i \n", maior);
+  printf("Posicao: Linha %i Coluna %i \n\n", linha, coluna);
+
+  system("pause");
 
   return 0;
 }
